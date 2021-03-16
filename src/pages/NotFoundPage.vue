aeb16d119388085125c2785b9db9ce49abb6c75f
kubemq <lior.nabat@kubemq.io>
2021-03-16 15:31:09 +0200

    update 404

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -1,26 +1,30 @@
 <template>
   <div
-    class="px-4 pb-4 d-flex flex-grow-1 flex-column justify-start align-start align-content-space-between"
+    class="d-flex flex-grow-1 justify-space-between align-center align-content-space-between"
   >
+    <v-spacer></v-spacer>
     <div class="col-12 pb-0">
-      <v-card flat class="text-center w-full error-page pa-3">
-        <v-img
-          src="/assets/images/404-illustration.svg"
-          max-height="400"
-          contain
-        />
-        <div class="display-2 mt-10">How did you get here?</div>
-        <div class="mt-3 mb-6">
-          Sorry we can't seem to find the page you're looking for.
+      <div class="d-flex justify-end align-content-center align-center col-12">
+        <v-spacer></v-spacer>
+        <div class="col-5 pr-4">
+          <v-img src="@/assets/ship.svg" alt="ship" />
         </div>
-        <v-btn to="/" block large color="primary">Send me Home</v-btn>
-      </v-card>
+        <div class="col-5 pl-4">
+          <v-card flat class="text-center  w-full">
+            <div class="display-2 secondary--text mt-10">
+              How did you get here?
+            </div>
+            <div class="mt-3 mb-6 secondary--text">
+              Sorry we can't seem to find the page you're looking for.
+            </div>
+            <v-btn text rounded to="/" color="primary">SEND ME HOME</v-btn>
+          </v-card>
+        </div>
+        <v-spacer></v-spacer>
+      </div>
+      <v-spacer></v-spacer>
     </div>
   </div>
 </template>
 <script></script>
-<style>
-.error-page {
-  max-width: 500px;
-}
-</style>
+<style></style>
